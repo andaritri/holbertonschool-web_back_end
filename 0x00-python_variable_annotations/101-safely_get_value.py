422fac7a0772c19379d23f0c51cdf911e5bbbdeb
andaritri <andresat0712@gmail.com>
2022-02-01 00:24:33 +0000

    Safe get value

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""File that contains safely_get_value function"""
+
+
+from typing import Any, Mapping, TypeVar, Union
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+    dct: Mapping,
+    key: Any,
+    default: Union[~T, None]
+) -> Union[Any, ~T]:
+    """return values, add type annotations to the function"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
