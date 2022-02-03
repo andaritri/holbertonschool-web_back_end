e6508fad9f726dba97a9caa9791843e047cfc4ab
andaritri <andresat0712@gmail.com>
2022-02-03 03:23:15 +0000

    Change return type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,6 +6,6 @@ from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[List[float], None, None]:
+async def async_comprehension() -> List[float]:
     """async comprehension function"""
     return [i async for i in async_generator()]
