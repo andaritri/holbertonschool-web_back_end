a37bcc5bb1d7f1577584da941a85dfb74d58a429
andaritri <andresat0712@gmail.com>
2022-02-16 01:34:15 +0000

    Extract authorization header

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,15 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic Authentication class
     """
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        """ Extract authorization header"""
+        if (
+            not authorization_header or
+            not isinstance(authorization_header, str) or
+            not authorization_header.startswith("Basic ")
+        ):
+            return None
+        return authorization_header.split(maxsplit=1)[-1]
