8b717858bd76f74a59856abe290dffda68f30bf3
andaritri <andresat0712@gmail.com>
2022-02-16 00:20:08 +0000

    Complete method

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,12 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """List path that require authentication
         """
-        return False
+        if path is None or not excluded_paths:
+            return True
+        for pth in excluded_paths:
+            if path in pth:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Autorization header
